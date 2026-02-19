5eab0abef175c852a44797ac53f668ac2fe65bcd
mileshko <mileshko.petr@yandex.ru>
2026-02-19 08:48:57 +0300

    редактирование комментариев / добавлена c_3

--- с_2/main_5.c
+++ с_2/main_5.c
@@ -7,9 +7,9 @@
 
 int main()
 {
-  printf("TABLE OF TEMPERATURE\n"); // Заголовок таблицы
+    printf("TABLE OF TEMPERATURE\n"); // Заголовок таблицы
 
-  /* Для вывода используем цикл for */
-  for (int fahr = 300; fahr >= 0; fahr -= 20)
-    printf("%3d\t%6.1f\n", fahr, 5.0 * (fahr - 32.0) / 9.0);
+    /* Для вывода используем цикл for */
+    for (int fahr = 300; fahr >= 0; fahr -= 20)
+        printf("%3d\t%6.1f\n", fahr, 5.0 * (fahr - 32.0) / 9.0);
 }
