acd16a4fece45b7236c3018f83db541dc622d61c
mileshko <mileshko.petr@yandex.ru>
2026-02-19 15:06:00 +0300

    редактирование c_4/main.c

--- с_4/main.c
+++ с_4/main.c
@@ -7,6 +7,17 @@
 
 int main(void)
 {
+    double nc = 0;
+    int c;
 
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\n')
+        {
+            nc++;
+        }
+    }
+
+    printf("%.0f", nc);
     return 0;
 }
