b99d96b1ccf6a9612d50edbfc41e5066666d7675
Peter <mileshko.petr@yandex.ru>
2026-02-24 19:08:27 +0300

    Выбор локали и рефакторинг кода
    улучшена программа из c_4/main_2.c

--- с_5/main.c
+++ с_5/main.c
@@ -9,34 +9,40 @@
  *  Сообщения в терминале на кириллице согласно заданию в readme.md
  */
 
- int main(void) {
+int main(void)
+{
 
     bool locale_status = true; // флаг локали по умолчанию true
 
-    int c, // текущий введённый пользователем символ с клавиатуры
+    int c,             // текущий введённый пользователем символ с клавиатуры
         last_c = '\n'; // последний введённый символ с клавиатуры
 
     long total_chars = 0, // счётчик символов
-        total_lines = 0, // счётчик строк
-        total_words = 0; // счётчик слов
+        total_lines = 0,  // счётчик строк
+        total_words = 0;  // счётчик слов
 
-    // Пытаемся переключить русскую локаль UTF-8 
-    if(setlocale(LC_ALL, "ru_RU.UTF8") == NULL) {
+    // Пытаемся переключить русскую локаль UTF-8
+    if (setlocale(LC_ALL, "ru_RU.UTF8") == NULL)
+    {
         // Пробуем альтернативный вариант
-        if(setlocale(LC_ALL, ".UTF8") == NULL) {
+        if (setlocale(LC_ALL, ".UTF8") == NULL)
+        {
             locale_status = false; // Не получилось - сброс флага локали
-            setlocale(LC_ALL,""); // Использовать системную локаль
+            setlocale(LC_ALL, ""); // Использовать системную локаль
         }
     }
 
-    if(locale_status) {
+    if (locale_status)
+    {
         // Если удалось установить русскую локаль UTF-8
         // выводим инструкцию пользователя на кириллице
         printf("Для завершения ввода:\n");
         printf("1. Нажать комбинацию Ctrl + X\n");
         printf("2. Нажать клавишу Enter\n");
         printf("Начните ввод текста >>>");
-    } else {
+    }
+    else
+    {
         // вывод инструкции пользователя на латинице
         // (английский язык или транслит)
         printf("To exit:\n");
@@ -47,16 +53,19 @@
 
     // Считываем нажатые символы на клавиатуре.
     // Выход из цикла при нажатии Ctrl-X (код 24)
-    while( (c = getchar()) != 24) {
+    while ((c = getchar()) != 24)
+    {
         total_chars++; // увеличиваем счетчик символов, учёт всех кроме Ctrl-x
 
-        if( c == '\n') {
+        if (c == '\n')
+        {
             // завершаем строку
-            total_lines++; 
+            total_lines++;
         }
 
-        if( (c == ' ' || c == '\t' || c == '\n') &&
-            !(last_c == ' ' || last_c == '\t' || last_c == '\n') ) {
+        if ((c == ' ' || c == '\t' || c == '\n') &&
+            !(last_c == ' ' || last_c == '\t' || last_c == '\n'))
+        {
             // Завершаем слово при условии,
             // что перед этим не было символа, указанного в условии
             total_words++;
@@ -64,36 +73,43 @@
 
         last_c = c; // Запомнить последний символ
     }
-    
-     // Учёт последнего незавершённого слова
-    if(!(last_c == ' ' || last_c == '\t' || last_c == '\n') && total_chars > 0)
+
+    // Учёт последнего незавершённого слова
+    if (!(last_c == ' ' || last_c == '\t' || last_c == '\n') && total_chars > 0)
     {
         total_words++;
     }
 
     // Учёт последней незавершённой строки
-    if(last_c != '\n' && total_chars > 0)
+    if (last_c != '\n' && total_chars > 0)
     {
         total_lines++;
     }
-    
+
     // Обработать если ошибка при вводе
-    if(ferror(stdin)) {
-        if(locale_status) {
+    if (ferror(stdin))
+    {
+        if (locale_status)
+        {
             perror("Ошибка ввода.");
-        } else {
+        }
+        else
+        {
             perror("Input error.");
         }
         return 1; // Завершить программу
     }
 
-    if(locale_status) {
+    if (locale_status)
+    {
         // Вывод таблицы на кириллице
         printf("\n%s\t\t%s\n", "СЧЁТЧИК", "ИТОГО");
         printf("\n%s\t%ld", "СИМВОЛОВ", total_chars);
         printf("\n%s\t\t%ld", "СТРОК", total_lines);
         printf("\n%s\t\t%ld", "СЛОВ", total_words);
-    } else {
+    }
+    else
+    {
         // Вывод таблицы на латинице (английский язык или транслит)
         printf("\n%s\t\t%s\n", "COUNTER", "TOTAL");
         printf("\n%s\t\t%ld", "CHARS", total_chars);
@@ -102,4 +118,4 @@
     }
 
     return 0;
- }
+}
