a39afabed786d96c9fe03320c87703d3a8b5f5ee
mileshko <mileshko.petr@yandex.ru>
2026-02-24 09:31:45 +0300

    добавлена задача

---
+++ с_4/main_2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/*
+ *  Программа подсчёта символов, cтрок и слов.
+ *  Ввод с клавиатуры.
+ *  Вывод результата в терминал.
+ *  Окончание ввода на новой строке  -
+ *  комбинацией клавиш - Ctrl + Z и потом Enter
+ */
+
+
+ int main(void) {
+
+    long number_of_chars = 0,
+        number_of_lines = 0,
+        number_of_words = 0;
+
+    int c;
+
+        while((c = getchar() ) != EOF) {
+            number_of_chars++; // увеличить счётчик символов
+
+            switch(c) {
+                case ' ':
+                case '\t':  // символы пробела и табуляция разделяют слова
+                    number_of_words++; //  увеличить счётчик слов
+                break;
+                case '\n': // символ перевода/конца строки 
+                    number_of_lines++;  //  увеличить счётчик строк
+                    number_of_words++; //  увеличить счётчик слов
+                break;
+                default:
+                break;
+            }
+
+        }
+
+        // Форматируем и выводим на экран таблицу
+        printf("\n%8s\t%-8s\n", "COUNTER","TOTAL");
+        printf("\n%8s\t%-8ld","LINES", number_of_lines);
+        printf("\n%8s\t%-8ld","WORDS", number_of_words);
+        printf("\n%8s\t%-8ld\n","CHARS", number_of_chars);
+
+    return 0;
+ }
