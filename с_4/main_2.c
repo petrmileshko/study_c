a8da882cb20cfac194b432b9059e5f9dcc8ab0b0
Peter <mileshko.petr@yandex.ru>
2026-02-24 19:04:32 +0300

    Программа подсчёта символов, слов и строк, ввод с клавиатуры

--- с_4/main_2.c
+++ с_4/main_2.c
@@ -9,71 +9,75 @@
  *  комбинацией клавиш - Ctrl + Z и потом Enter (EOF)
  */
 
-
- int main(void) {
+int main(void)
+{
 
     long number_of_chars = 0,
-        number_of_lines = 0,
-        number_of_words = 0;
+         number_of_lines = 0,
+         number_of_words = 0;
 
-    int c, last_char;
+    int c, last_char = '\n';
     bool new_word = false; // флаг устанавливается в истину для каждого нового слова
-    
-        while((c = getchar() ) != EOF) {
-        
-            // Игнорируем символ Ctrl+Z (код 26) при вводе с клавиатуры,
-            //  когда комбинация нажата не на новой строке
-            if (c == 26) { // 26 — код символа Ctrl+Z в Windows
-                continue; // пропускаем этот символ, не увеличиваем счётчики
-            }
 
-            number_of_chars++; // увеличить счётчик символов
+    while ((c = getchar()) != EOF)
+    {
 
-            switch(c) {
-                case ' ':
-                case '\t':  // символ пробела и табуляции завершают слово
-                    if(new_word) // если флаг установлен надо
-                        number_of_words++; // увеличить счётчик слов
-                    new_word = false; // слово завершено, сброс флага и ожидание ввода нового слова
-                break;
-                case '\n': // конец строки 
-                    number_of_lines++; // увеличить счётчик строк
-                    if(new_word)
-                        number_of_words++; // символ \n также завершает слово
-                    new_word = false; // после сбросить флаг
-                break;
-                default:
-                // в случае ввода любых других символов, начинаем новое слово и
-                    new_word = true; // устанавливаем флаг
-                break;
-            }
-
-            last_char = c; // запомним последний символ
-
+        // Игнорируем символ Ctrl+Z (код 26) при вводе с клавиатуры,
+        //  когда комбинация нажата не на новой строке
+        if (c == 26)
+        {             // 26 — код символа Ctrl+Z в Windows
+            continue; // пропускаем этот символ, не увеличиваем счётчики
         }
 
-        // Обработать случай, если последнее слово не закончилось разделителем
-        if (new_word) {
-            number_of_words++;
-        }
+        number_of_chars++; // увеличить счётчик символов
 
-        // Обработать случай, если последняя строка не заканчивается символом \n
-        if (last_char != '\n' && number_of_chars > 0) {
-            number_of_lines++; // в этом случае увеличить количество строк
-        }
-
-        // обработаем если возникла ошибка при вводе
-        if (ferror(stdin))
+        switch (c)
         {
-             perror("Input Error");
-             return 1;
+        case ' ':
+        case '\t':                 // символ пробела и табуляции завершают слово
+            if (new_word)          // если флаг установлен надо
+                number_of_words++; // увеличить счётчик слов
+            new_word = false;      // слово завершено, сброс флага и ожидание ввода нового слова
+            break;
+        case '\n':             // конец строки
+            number_of_lines++; // увеличить счётчик строк
+            if (new_word)
+                number_of_words++; // символ \n также завершает слово
+            new_word = false;      // после сбросить флаг
+            break;
+        default:
+            // в случае ввода любых других символов, начинаем новое слово и
+            new_word = true; // устанавливаем флаг
+            break;
         }
 
-        // Форматируем и выводим на экран таблицу
-        printf("\n%8s\t%-8s\n", "COUNTER","TOTAL");
-        printf("\n%8s\t%-8ld","LINES", number_of_lines);
-        printf("\n%8s\t%-8ld","WORDS", number_of_words);
-        printf("\n%8s\t%-8ld\n","CHARS", number_of_chars);
+        last_char = c; // запомним последний символ
+    }
+
+    // Обработать случай, если последнее слово не закончилось разделителем
+    if (new_word)
+    {
+        number_of_words++;
+    }
+
+    // Обработать случай, если последняя строка не заканчивается символом \n
+    if (last_char != '\n' && number_of_chars > 0)
+    {
+        number_of_lines++; // в этом случае увеличить количество строк
+    }
+
+    // обработаем если возникла ошибка при вводе
+    if (ferror(stdin))
+    {
+        perror("Input Error");
+        return 1;
+    }
+
+    // Форматируем и выводим на экран таблицу
+    printf("\n%8s\t%-8s\n", "COUNTER", "TOTAL");
+    printf("\n%8s\t%-8ld", "LINES", number_of_lines);
+    printf("\n%8s\t%-8ld", "WORDS", number_of_words);
+    printf("\n%8s\t%-8ld\n", "CHARS", number_of_chars);
 
     return 0;
- }
+}
